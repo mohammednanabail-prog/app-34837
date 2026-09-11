82e6b61311b4c0ce8722bdb4fbe3bb04ad31c4e0
mohammednanabail-prog <mohammednanabail@gmail.com>
2026-09-11 23:29:12 +0300

    Add app/src/main/java/com/example/ui/theme/Color.kt

---
+++ app/src/main/java/com/example/ui/theme/Color.kt
@@ -0,0 +1,47 @@
+package com.example.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Geometric Balance Palette
+val PrimaryLight = Color(0xFF006494)
+val OnPrimaryLight = Color(0xFFFFFFFF)
+val PrimaryContainerLight = Color(0xFFD3E4FF)
+val OnPrimaryContainerLight = Color(0xFF041E49)
+
+val SecondaryLight = Color(0xFF50606E)
+val OnSecondaryLight = Color(0xFFFFFFFF)
+val SecondaryContainerLight = Color(0xFFD4E4F6)
+val OnSecondaryContainerLight = Color(0xFF0D1D2A)
+
+val BackgroundLight = Color(0xFFF7F9FC)
+val OnBackgroundLight = Color(0xFF191C1E)
+val SurfaceLight = Color(0xFFF7F9FC)
+val OnSurfaceLight = Color(0xFF191C1E)
+val SurfaceContainerLight = Color(0xFFF0F4F9)
+val SurfaceVariantLight = Color(0xFFE0E2E6)
+val OnSurfaceVariantLight = Color(0xFF43474E)
+val OutlineLight = Color(0xFF73777F)
+val OutlineVariantLight = Color(0xFFC3C7CE)
+
+// Dark Theme Colors for Geometric Balance
+val PrimaryDark = Color(0xFF8BCEFF)
+val OnPrimaryDark = Color(0xFF00344F)
+val PrimaryContainerDark = Color(0xFF004B73)
+val OnPrimaryContainerDark = Color(0xFFD3E4FF)
+
+val SecondaryDark = Color(0xFFB7C8D8)
+val OnSecondaryDark = Color(0xFF22323F)
+val SecondaryContainerDark = Color(0xFF384956)
+val OnSecondaryContainerDark = Color(0xFFD4E4F6)
+
+val BackgroundDark = Color(0xFF101417)
+val OnBackgroundDark = Color(0xFFE0E3E8)
+val SurfaceDark = Color(0xFF101417)
+val OnSurfaceDark = Color(0xFFE0E3E8)
+val SurfaceContainerDark = Color(0xFF1B1F23)
+val SurfaceVariantDark = Color(0xFF43474E)
+val OnSurfaceVariantDark = Color(0xFFC3C7CE)
+val OutlineDark = Color(0xFF8D9199)
+val OutlineVariantDark = Color(0xFF43474E)
+
+
